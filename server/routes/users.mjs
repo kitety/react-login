c42a333d5862b4279a0f852eaa593cff878b6736
kitety <aizaizuori@foxmail.com>
2018-11-18 23:26:18 +0800

    服务端唯一性验证

--- server/routes/users.mjs
+++ server/routes/users.mjs
@@ -3,9 +3,31 @@ import isEmpty from 'lodash/isEmpty'
 import validator from 'validator'
 import User from '../models/user.mjs'
 import bcrypt from 'bcrypt'
+import Promise from 'bluebird'
 
 let router = express.Router()
-const validataInput = (data) => {
+const validataInput = (data, otherValiddations) => {
+  let { errors } = otherValiddations(data);
+  return Promise.all([
+    User.where({ email: data.email }).fetch().then(user => {
+      if (user) {
+        errors.email = 'There is a such email'
+      }
+    }),
+    User.where({ username: data.username }).fetch().then(user => {
+      if (user) {
+        errors.username = 'There is a such username'
+      }
+    })
+  ]).then(() => {
+    return {
+      errors,
+      isValid: isEmpty(errors)
+    }
+  })
+}
+// 基础验证
+const commonValidataInput = (data) => {
   let errors = {}
   if (validator.isEmpty(data.username)) {
     errors.username = " The field is required."
@@ -28,19 +50,20 @@ const validataInput = (data) => {
   }
 }
 router.post('/', (req, res) => {
-  const { errors, isValid } = validataInput(req.body)
-  if (isValid) {
-    const { username, password, email } = req.body;
-    // 对密码十位hash加密
-    const password_digest = bcrypt.hashSync(password, 10);
-    // 存储,并且有时间戳
-    User.forge({
-      username, password_digest, email
-    }, { hasTimestamps: true }).save()
-      .then(() => { res.json({ success: true }) })
-      .catch(err => { res.status(404).json(err) })
-  } else {
-    return res.status(404).json(errors)
-  }
+  validataInput(req.body, commonValidataInput).then(({ errors, isValid }) => {
+    if (isValid) {
+      const { username, password, email } = req.body;
+      // 对密码十位hash加密
+      const password_digest = bcrypt.hashSync(password, 10);
+      // 存储,并且有时间戳
+      User.forge({
+        username, password_digest, email
+      }, { hasTimestamps: true }).save()
+        .then(() => { res.json({ success: true }) })
+        .catch(err => { res.status(404).json(err) })
+    } else {
+      return res.status(404).json(errors)
+    }
+  })
 })
 export default router;
